d8742ab88d8bab16c17de97f4e124414e5819194
ANdRoId5124 <jkj43483@gmail.com>
2022-05-16 20:52:47 +0200

    MultyThreadingHM

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread2 = new Thread(new Thread2());
+        Thread thread3 = new Thread(new Thread3());
+        Thread thread1 = new Thread(new Thread1());
+        
+       thread3.start();
+
+        thread2.start();
+        thread2.join();
+
+        thread1.start();
+        thread1.join();
+
+
+
+
+
+    }
+}
---
+++ src/Thread3.java
@@ -0,0 +1,6 @@
+public class Thread3 implements Runnable{
+    @Override
+    public void run() {
+        System.out.println("Thread3");
+    }
+}
